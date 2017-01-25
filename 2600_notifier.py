caeedc1c6857e51d38bc120b1a33082709c8cfac
mcraze123 <mcraze123@gmail.com>
2017-01-25 09:21:35 -0500

    fixed regex to match more exact title

--- 2600_notifier.py
+++ 2600_notifier.py
@@ -8,7 +8,7 @@ fh = open(f, 'r+')
 line = fh.readline()
 line = line.strip()
 if line != d['entries'][0]['link']:
-    if re.match(r'(WINTER|SPRING|SUMMER|AUTUMN).*', d['entries'][0]['title']):
+    if re.match(r'(WINTER|SPRING|SUMMER|AUTUMN)\s+ISSUE\s+OF\s+2600\s+RELEASED', d['entries'][0]['title']):
         print d['entries'][0]['title'] + " " + d['entries'][0]['link'] + "\n"
 fh.seek(0)
 fh.truncate()
